b21d763f38c76b9f7d36cd86183484047e5137d6
Masta-isshakh <mastaisshakh@gmail.com>
2025-10-16 04:35:12 -0700

    first commit1

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { storage } from './storage/resource';
+import { firstBucket, secondBucket } from './storage/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -9,5 +9,6 @@ import { storage } from './storage/resource';
 defineBackend({
   auth,
   data,
-  storage,
+  firstBucket,
+  secondBucket,
 });
--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,15 @@
 import { defineStorage } from "@aws-amplify/backend";
 
-export const storage=defineStorage({
-    name:'amplifyTeamDrive'
+export const firstBucket=defineStorage({
+    name:'firstBucket',
+    isDefault:true,
 });
+
+export const secondBucket=defineStorage({
+    name:'secondBucket',
+    access:(allow)=>({
+        'private/{entity_id}/*':[
+            allow.entity('identity').to(['read', 'write', 'delete'])
+        ]
+    })
+})
