b6b419441865262f05ad67c29901bd677e187ad3
Masta-isshakh <mastaisshakh@gmail.com>
2025-10-22 08:20:16 -0700

    first commit s3

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,22 +1,19 @@
-// amplify/data/resource.ts
-import { a, defineData } from "@aws-amplify/backend";
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
 const schema = a.schema({
-  Post: a
+  Todo: a
     .model({
-      title: a.string().required(),       // Titre de la publication
-      imagePath: a.string().required(),   // Chemin du fichier S3
-      author: a.string(),                 // Nom du créateur (optionnel)
-      createdAt: a.datetime().required(), // Date de création
+      content: a.string(),
+      isDone: a.boolean()
     })
-    .authorization((allow) => [
-      allow.publicApiKey(), // tout le monde peut lire
-      allow.owner(),        // seul le propriétaire peut écrire
-    ]),
+    .authorization(allow => [allow.owner()])
 });
+
+export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'userPool'
+  }
 });
-
-export type Schema = typeof schema;
